061303a4d1454af3e8f774dd08cd9cd144761c0e
Chikinov Maxim <m.chikinov@growapp.biz>
2022-06-10 00:16:42 +0300

    Card

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            VStack {
+                Spacer()
+            }
+            .frame(width: 300, height: 220)
+            .background(.blue)
+            .cornerRadius(20)
+            .shadow(radius: 20)
+            .offset(x: 0, y: -10)
+            
+            VStack(alignment: .center, spacing: 20) {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("Doggy Card")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .multilineTextAlignment(.leading)
+                        Text("Pay it, beach!")
+                            .font(.body)
+                            .fontWeight(.regular)
+                            .foregroundColor(.accentColor)
+                            .multilineTextAlignment(.leading)
+                    }
+                    Spacer()
+                    Image("Logo1")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 40, height: 40, alignment: .center)
+                }
+                .padding(.horizontal, 20)
+                .padding(.top, 20)
+                Image("Card1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: 110, alignment: .top)
+                    .padding(.horizontal, 20)
+            }
+            .frame(width: 340, height: 200)
+            .background(.black)
+            .cornerRadius(20)
+            .shadow(radius: 20)
+        }
     }
 }
 
