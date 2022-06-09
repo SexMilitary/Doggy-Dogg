8b5ce32c8d658f566dff72f8bbe07b40dc16c3a1
Chikinov Maxim <m.chikinov@growapp.biz>
2022-06-09 23:43:55 +0300

    init

---
+++ Shared/DoggyDoggApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoggyDoggApp.swift
+//  Shared
+//
+//  Created by Максим Чикинов on 09.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct DoggyDoggApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
